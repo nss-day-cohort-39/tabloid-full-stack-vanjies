676d41f9e20bc6353163074df42f5dd3556889af
Hellowalls1 <thesingingbutcher@gmail.com>
2020-07-11 15:35:00 -0500

    fixed the grandchild issue

--- Tabloid/client/src/components/PostDetails.js
+++ Tabloid/client/src/components/PostDetails.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PostContext } from "../providers/PostProvider";
-//using the Card component that comes with reactstrap to organize some of the post details
+
+
 const PostDetail = () => {
     const { getPostById } = useContext(PostContext)
-    const [post, setPost] = useState({});
+    const [post, setPost] = useState();
     const { id } = useParams();
 
     useEffect(() => {
         getPostById(id).then(setPost);
     }, []);
 
+    if (!post) {
+        return null;
+    }
 
 
     return (
@@ -19,7 +23,7 @@ const PostDetail = () => {
                 <div className="col-sm-12 col-lg-6">
                     <p className="post-details-image">{post.imageLocation}</p>
                     <p className="post-details-title">{post.title}</p>
-                    <p className="text-left px-2">Posted by: {post.userProfile.DisplayName}</p>
+                    <p className="text-left px-2">Posted by: {post.userProfile.displayName}</p>
                     <p className="post-details-content">{post.content}</p>
                     <p className="post-details-publishDate">{post.publishDateTime}</p>
                 </div>
